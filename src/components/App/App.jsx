15a9c88efada18539a4433e16b1c938ca582bf7a
first-phoenix <iswarajkhan@gmail.com>
2021-06-19 02:18:18 +0530

    state and prop learnt and implemented, also searchbox is created

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
+import SearchBox from '../SearchBox/SearchBox';
+
 
 class App extends React.Component {
+
+    state = {
+        headerText: 'Name It',
+    }
 
     render() {
 
         return (
             <div>
-                <Header />
+                <Header headertxt={this.state.headerText} />
+                <SearchBox />
+
+
+                {/* <h3>{this.state.headerText}</h3>
+                <button onClick={() => {
+
+                    this.setState({
+                        headerText: 'woaaaaaaah!',
+                    });
+                }
+
+                }>
+                    Magic Happens here
+                </button> */}
             </div>
         )
     }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Header.css';
 
-const Header = () => {
+const Header = (props) => {
     return (
         <div className="head-container">
             <img src="https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png"
                 className="head-image"
                 alt="Header" />
-            <h1 className="head-text">Name It</h1>
+            <h1 className="head-text">{props.headertxt}</h1>
         </div>
     )
 }
