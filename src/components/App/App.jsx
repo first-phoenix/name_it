d7c2745607f0e44bf9465cc06ba2595261135050
first-phoenix <iswarajkhan@gmail.com>
2021-06-19 13:14:08 +0530

    After part 9, ccreated animation of header image and text

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,14 +8,26 @@ class App extends React.Component {
 
     state = {
         headerText: 'Name It',
+        headerExpanded: true
+    }
+
+    handleInputChange = (inputText) => {
+        // console.log('The input text is - ', inputText);
+        // this.setState({ headerExpanded: inputText.length > 0 ? false : true });
+        // this.setState({ headerExpanded: !(inputText.length > 0) });
+        this.setState({ headerExpanded: !inputText });
+
     }
 
     render() {
 
         return (
             <div>
-                <Header headertxt={this.state.headerText} />
-                <SearchBox />
+                <Header
+                    headerExpanded={this.state.headerExpanded}
+                    headertxt={this.state.headerText}
+                />
+                <SearchBox onInputChange={this.handleInputChange} />
 
 
                 {/* <h3>{this.state.headerText}</h3>
